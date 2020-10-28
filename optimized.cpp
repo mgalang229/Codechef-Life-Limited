d87791e350e3c28abe8eecee7b21930e7e02e3a0
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-10-28 16:13:28 +0800

    Create optimized.cpp

---
+++ optimized.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin >> t;
+	while(t--) {
+		char a[3][3];
+		for(int i=0; i<3; ++i)
+			for(int j=0; j<3; ++j)
+				cin >> a[i][j];
+		bool ok=0;
+		for(int i=0; i<3; ++i)
+			for(int j=0; j<3; ++j)
+				if(i<2&&j<2&&a[i][j]=='l'&&a[i+1][j]=='l'&&a[i+1][j+1]=='l')
+					ok=1;
+		cout << (ok?"yes":"no") << "\n";
+	}
+}
